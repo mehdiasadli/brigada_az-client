21d696208fe92b9d66aac88ff50729c536da991e
Mehdi Asadli <mehdiasadli@Mehdis-MacBook-Pro.local>
2024-07-18 17:29:26 +0400

    fix .at prob.

--- src/components/ui/ProfileNavItem.tsx
+++ src/components/ui/ProfileNavItem.tsx
@@ -1,6 +1,6 @@
 import { ActionIcon, Tooltip } from '@mantine/core';
 import { Icon as TablerIcon, IconProps } from '@tabler/icons-react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const ProfileNavItem = ({
   name,
@@ -11,16 +11,9 @@ const ProfileNavItem = ({
   Icon: React.ForwardRefExoticComponent<IconProps & React.RefAttributes<TablerIcon>>;
   to: string;
 }) => {
-  const { pathname } = useLocation();
-  const isActive = pathname.endsWith(to.split('/').at(-1)!);
-
   return (
     <Tooltip label={name}>
-      <ActionIcon
-        size='xl'
-        variant={isActive ? 'filled' : 'outline'}
-        renderRoot={(props) => <Link {...props} to={to} />}
-      >
+      <ActionIcon size='xl' variant={'outline'} renderRoot={(props) => <Link {...props} to={to} />}>
         <Icon size={15} />
       </ActionIcon>
     </Tooltip>
