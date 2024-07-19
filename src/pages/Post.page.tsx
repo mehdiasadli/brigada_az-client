43fbdaa5cd33d15155a6b19a8770636b0eb13778
Mehdi Asadli <mehdiasadli@Mehdis-MacBook-Pro.local>
2024-07-19 17:19:08 +0400

    fix: white screen after post deletion

--- src/pages/Post.page.tsx
+++ src/pages/Post.page.tsx
@@ -2,15 +2,34 @@ import { useParams } from 'react-router-dom';
 import { usePostDetails } from '../api/post/queries';
 import Post from '../components/ui/Post';
 import MetaTitle from '../components/ui/MetaTitle';
+import { Center, Loader, Title } from '@mantine/core';
 
 const PostPage = () => {
   const { id } = useParams() as { id: string };
   const { data, status, error } = usePostDetails(id);
 
+  if (status === 'pending') {
+    return (
+      <Center mt={10}>
+        <Loader type='dots' />
+      </Center>
+    );
+  }
+
+  if (status === 'error') {
+    return (
+      <Center mt={10}>
+        <Title order={6} c='red'>
+          {error?.response?.data?.message || 'Error Occured'}
+        </Title>
+      </Center>
+    );
+  }
+
   return (
     <>
       <MetaTitle title={data?.content?.slice(0, 20)} />
-      <Post post={data!} status={status} error={error} details />
+      {data && <Post post={data} status={status} error={error} details />}
     </>
   );
 };
