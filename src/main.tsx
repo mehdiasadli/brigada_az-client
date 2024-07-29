108b3f2988147280f69f27a958fcea2a30ee8040
Mehdi Asadli <mehdiasadli@Mehdis-MacBook-Pro.local>
2024-07-29 12:13:18 +0400

    add refresh to ios standalone apps

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,20 @@
+import PullToRefresh from 'pulltorefreshjs';
 import ReactDOM from 'react-dom/client';
 import App from './app';
 import { inject } from '@vercel/analytics';
 import './index.css';
 
 inject();
+
+const standalone = window.matchMedia('(display-mode: standalone)').matches;
+const ios = /iPhone|iPad|iPod/i.test(window.navigator.userAgent);
+
+if (standalone && ios) {
+  PullToRefresh.init({
+    onRefresh() {
+      window.location.reload();
+    },
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
